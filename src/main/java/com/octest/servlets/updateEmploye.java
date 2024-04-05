9202b9e4ab9b4e756a1db797f5ed215beb1e9add
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-04-05 15:44:36 +0000

    ajouter des petites détails sur des servlettes

--- src/main/java/com/octest/servlets/updateEmploye.java
+++ src/main/java/com/octest/servlets/updateEmploye.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.octest.beans.GestionEmployes;
+
 /**
  * Servlet implementation class updateEmploye
  */
@@ -33,6 +35,16 @@ public class updateEmploye extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("nom");
+		String email = request.getParameter("email");
+		int telephone = Integer.parseInt(request.getParameter("telephone"));
+		String departement = request.getParameter("departement");
+		String poste = request.getParameter("poste");
+		
+		GestionEmployes gestionEM = new GestionEmployes();
+		boolean update = gestionEM.updateEmploye(nom, email, telephone, departement, poste);
+		
+		request.setAttribute("Resultat", update);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/showEmploye.jsp").forward(request, response);
 	}
 
